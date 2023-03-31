e5f2bdd03903660b22fbd6e690be07f0c7e5441b
unknown <harshiit.anand@gmail.com>
2023-03-31 22:33:17 +0530

    Completed Template driven form

--- src/app/templatedriven/templatedriven.component.ts
+++ src/app/templatedriven/templatedriven.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-templatedriven',
@@ -7,7 +7,18 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./templatedriven.component.css']
 })
 export class TemplatedrivenComponent implements OnInit {
-
+  defaultQuestion='pet';
+  myvar='';
+  genders=['male', 'female'];
+  suggestVar='';
+  suggest(){
+     // s1.value=this.suggestVar;
+     this.suggestVar='Harshit';
+  }
+  clear(){
+    // s1.value=this.suggestVar;
+    this.suggestVar='';
+ }
   constructor() { }
 
   ngOnInit(): void {
@@ -16,6 +27,25 @@ export class TemplatedrivenComponent implements OnInit {
     console.log(form);
     console.log('email is' +form.value.email);
     form.value.email='xyz@gmail.com';
+    if(form.dirty)
+    alert("dirty");
+  }
+//f1 is template variable of second form
+  @ViewChild('f1') signupForm? :NgForm;
+  onSubmit2(){
+        console.log(this.signupForm);
   }
 
+  user={
+    username:'',
+    email: ''
+  };
+  @ViewChild('f3') signupForm2? :NgForm;
+  submitted=false;
+  onSubmit3(){
+    this.submitted=true;
+      this.user.username=this.signupForm2?.value.username;
+      this.user.email=this.signupForm2?.value.email;
+  }
 }
+
